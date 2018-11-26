cb915f0925e7f2d1a7155f8141c81a414a5db345
toxithom <toxithom@users.noreply.github.com>
2018-11-26 07:06:31 +0100

    add test file BruteRasterImageTest

---
+++ src/test/java/image/BruteRasterImageTest.java
@@ -0,0 +1,70 @@
+package image;
+
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BruteRasterImageTest {
+
+  private static int width = 500, height = 800;
+
+  private static Color[][] zeroDimensionMatrix;
+
+  private static Color[][] nullFilledMatrix;
+
+  private static Color[][] goldMatrix, blueMatrix;
+
+  private Image goldImage, blueImage;
+
+  @BeforeAll
+  static void init () {
+    zeroDimensionMatrix = new Color[0][0];
+    nullFilledMatrix = new Color[height][width];
+    goldMatrix = new Color[height][width];
+    blueMatrix = new Color[height][width];
+    for (int row = 0; row < height; row++) {
+      for(int col = 0; col < width; col++) {
+        goldMatrix[row][col] = Color.GOLD;
+        blueMatrix[row][col] = Color.BLUEVIOLET;
+      }
+    }
+  }
+
+  @BeforeEach
+  void setUp () {
+    goldImage = new BruteRasterImage(goldMatrix);
+    blueImage = new BruteRasterImage(blueMatrix);
+  }
+
+  @Test
+  void testConstructor () {
+    assertThrows(RuntimeException.class, () -> new BruteRasterImage(zeroDimensionMatrix));
+    assertThrows(RuntimeException.class, () -> new BruteRasterImage(nullFilledMatrix));
+    assertThrows(RuntimeException.class, () -> new BruteRasterImage(null, width, height));
+    assertNotNull(new BruteRasterImage(goldMatrix));
+    assertNotNull(new BruteRasterImage(Color.GOLD, width, height));
+  }
+
+  @Test
+  void getPixelColor () {
+    for (int row = 0; row < height; row++) {
+      for (int col = 0; col < width; col++) {
+        assertEquals(Color.GOLD, goldImage.getPixelColor(row, col));
+        assertEquals(Color.BLUEVIOLET, blueImage.getPixelColor(row, col));
+      }
+    }
+  }
+
+  @Test
+  void getWidth () {
+    assertEquals(width, goldImage.getWidth());
+  }
+
+  @Test
+  void getHeight () {
+    assertEquals(height, goldImage.getHeight());
+  }
+}
