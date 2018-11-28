e4ea82b2a75c8c2c2702491bff5b3a526289997c
toxithom <toxithom@users.noreply.github.com>
2018-11-28 15:26:13 +0100

    update test

--- src/test/java/image/RasterImageTest.java
+++ src/test/java/image/RasterImageTest.java
@@ -15,33 +15,41 @@ class RasterImageTest {
 
   private static Color[][] nullFilledMatrix;
 
-  private static Color[][] goldMatrix, blueMatrix, blackmatrix;
+  private static Color[][] goldMatrix, blueMatrix, redMatrix,  blackmatrix, whiteMatrix;
 
   private BruteRasterImage goldImage, blueImage;
 
   private PaletteRasterImage blackImage;
 
+  private SparseRasterImage whiteImage, redImage;
+
   @BeforeAll
   static void init () {
     zeroDimensionMatrix = new Color[0][0];
     nullFilledMatrix = new Color[height][width];
+    goldMatrix = new Color[height][width];
+    blueMatrix = new Color[height][width];
+    blackmatrix = new Color[height][width];
+    whiteMatrix = new Color[height][width];
+    redMatrix = new Color[height][width];
   }
 
   @BeforeEach
   void setUp () {
-    goldMatrix = new Color[height][width];
-    blueMatrix = new Color[height][width];
-    blackmatrix = new Color[height][width];
     for (int row = 0; row < height; row++) {
       for(int col = 0; col < width; col++) {
         goldMatrix[row][col] = Color.GOLD;
         blueMatrix[row][col] = Color.BLUEVIOLET;
         blackmatrix[row][col] = Color.BLACK;
+        whiteMatrix[row][col] = Color.WHITESMOKE;
+        redMatrix[row][col] = Color.RED;
       }
     }
     goldImage = new BruteRasterImage(goldMatrix);
     blueImage = new BruteRasterImage(blueMatrix);
     blackImage = new PaletteRasterImage(blackmatrix);
+    whiteImage = new SparseRasterImage(whiteMatrix);
+    redImage = new SparseRasterImage(redMatrix);
   }
 
   @Test
@@ -61,6 +69,8 @@ class RasterImageTest {
         assertEquals(Color.GOLD, goldImage.getPixelColor(col, row));
         assertEquals(Color.BLUEVIOLET, blueImage.getPixelColor(col, row));
         assertEquals(Color.BLACK, blackImage.getPixelColor(col, row));
+        assertEquals(Color.WHITESMOKE, whiteImage.getPixelColor(col, row));
+        assertEquals(Color.RED, redImage.getPixelColor(col, row));
       }
     }
   }
@@ -82,9 +92,13 @@ class RasterImageTest {
         goldImage.setPixelColor(blueImage.getPixelColor(col, row), col, row);
         blueImage.setPixelColor(Color.RED, col, row);
         blackImage.setPixelColor(Color.YELLOWGREEN, col, row);
+        whiteImage.setPixelColor(Color.CYAN, col, row);
+        redImage.setPixelColor(Color.CRIMSON, col, row);
         assertEquals(Color.BLUEVIOLET, goldImage.getPixelColor(col, row));
         assertEquals(Color.RED, blueImage.getPixelColor(col, row));
         assertEquals(Color.YELLOWGREEN, blackImage.getPixelColor(col, row));
+        assertEquals(Color.CYAN, whiteImage.getPixelColor(col, row));
+        assertEquals(Color.CRIMSON, redImage.getPixelColor(col, row));
       }
     }
   }
